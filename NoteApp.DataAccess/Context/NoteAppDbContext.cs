0f28b16beff8e4c8ece657fc63c4f0df430366f2
Irakli Jashi <iraklijashi0@gmail.com>
2024-09-25 18:59:08 +0400

    Data Access part finished.

---
+++ NoteApp.DataAccess/Context/NoteAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteApp.Data.Entities;
+
+namespace NoteApp.Data.Context;
+
+public class NoteAppDbContext : DbContext
+{
+    public NoteAppDbContext(DbContextOptions<NoteAppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Note> Notes { get; set; }
+}
---
+++ NoteApp.DataAccess/Entities/Note.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoteApp.Data.Entities;
+
+public class Note
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Title { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    [MaxLength(20)]
+    public string Phone { get; set; }
+}
